6c419c912609b02995d40221423f5c3d37b7bdf3
Ramon Valerio da Silva <rvalerio@tecgraf.puc-rio.br>
2017-08-18 18:03:20 -0300

    alguns ajustes

--- ExpressaoDinamica.Application/Service/ExpressaoService.cs
+++ ExpressaoDinamica.Application/Service/ExpressaoService.cs
@@ -9,6 +9,7 @@ namespace ExpressaoDinamica.Application.Service
     public class ExpressaoService : IExpressaoService
     {
         private readonly ScriptEngine _engine = Python.CreateEngine();
+
         public TNumber CalcularExpressaoFromPython<TNumber>(string expressao)
         {
             return _engine.CreateScriptSourceFromString(expressao, SourceCodeKind.Expression).Execute<TNumber>();
@@ -20,7 +21,7 @@ namespace ExpressaoDinamica.Application.Service
         }
         public double CalcularExpressaoFromCSharp(string expressao)
         {
-            return 2 + (3 * 5);
+            return Convert.ToDouble(2 + (3 * 5));
         }
     }
 }
